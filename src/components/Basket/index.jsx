7c1869e7eb479a74e64b17cd5227e36601dbd5a2
bermet-tugolova <btugolova@gmail.com>
2021-08-18 17:43:00 +0600

    basket func

--- src/components/Basket/index.jsx
+++ src/components/Basket/index.jsx
@@ -5,12 +5,14 @@ import { useSelector } from 'react-redux'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { Context } from '../../index'
 import { useDispatch } from 'react-redux'
-import { add_order, delete_order, remove } from '../../store/actions'
+import {
+	decrement,
+	deleteProduct,
+	increment,
+} from '../../store/actions'
 
 export const Basket = () => {
 	const orders = useSelector((state) => state.orders)
-	const totalPrice = useSelector((state) => state.totalPrice)
-	const totalCount = useSelector((state) => state.totalCount)
 	const { auth } = useContext(Context)
 	const [user] = useAuthState(auth)
 	const dispatch = useDispatch()
@@ -35,9 +37,9 @@ export const Basket = () => {
 											<button
 												className='btn'
 												onClick={() => {
-													if (totalCount !== 1) {
+													if (order.count !== 1) {
 														dispatch(
-															delete_order(order),
+															decrement(order.id),
 														)
 													} else {
 														return null
@@ -46,11 +48,11 @@ export const Basket = () => {
 											>
 												-
 											</button>
-											<p className='num'>{totalCount}</p>
+											<p className='num'>{order.count}</p>
 											<button
 												className='btn'
 												onClick={() =>
-													dispatch(add_order(order))
+													dispatch(increment(order.id))
 												}
 											>
 												+
@@ -60,13 +62,15 @@ export const Basket = () => {
 									<div className='basket_solution'>
 										<p>
 											{Math.floor(
-												totalCount * totalPrice,
+												order.count * order.price,
 											)}
 											$
 										</p>
 									</div>
 									<div
-										onClick={() => dispatch(remove(index))}
+										onClick={() =>
+											dispatch(deleteProduct(index))
+										}
 										className='delete'
 									>
 										Удалить
@@ -77,9 +81,11 @@ export const Basket = () => {
 					})
 				) : (
 					<div className='basket'>
-						<h1 className='korz'>There is nothing in the basket yet</h1>
+						<h1 className='korz'>
+							There is nothing in the basket yet
+						</h1>
 						<p>
-						Start from the home page to find something specific
+							Start from the home page to find something specific
 						</p>
 						<Link to='/'>
 							<button className='btn'>Go to Main page</button>
